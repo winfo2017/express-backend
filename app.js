d3c87a2fe120a707806df81a7080cda4c01eda86
Ethan Anderson <aethan@uw.edu>
2017-01-28 18:07:07 -0800

    something somethign horses

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var util = require('util');
 
 var users;
-fs.readFile('file', 'utf8', function (err, data) {
+fs.readFile('fake.json', 'utf8', function (err, data) {
   if (err) throw err;
   users = JSON.parse(data);
 });
@@ -14,13 +14,34 @@ fs.readFile('file', 'utf8', function (err, data) {
 
 // return a user in the users object
 app.get('/users/:userId', function (req, res) {
-    return users[req.params.userId];
+    res.send(users.users[req.params.userId]);
     //res.send(req.params)
 })
 
-function getUser(){
+// get a list of all the users previous matches
+app.get('/users/:userId/matches/', function(req, res){
+    // find all the matches for a user and return as an array
+    var matches = users.users[req.params.userId]['matches'];
+    var matchReturn = [];
+    
+    matches.forEach(function(match){
+        matchReturn.push(users.users[match]);
+    });
+    res.send(matchReturn);
+});
 
-}
+// get a list of new people to swipe on
+app.get('/users/:userId/swipes', function(req, res){
+    // get their matches so we can compare against to return non matched users
+    var matches = users.users[req.params.userId]['matches'];
+    var swipes = [];
+    for(var key in users['users']){
+        if(!matches.includes(key)){
+            swipes.push(users.users[key])
+        }
+    }
+    res.send(swipes);
+});
 
 app.listen(3000, '0.0.0.0', function() {
     console.log('Listening to port:  ' + 3000);
